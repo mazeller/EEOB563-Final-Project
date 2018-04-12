c7a13203cc08ced588f3275bbf9b38563451308a
Michael Zeller <mazeller@iastate.edu>
2018-04-11 20:53:00 -0500

    Adding bayesian final run

---
+++ results/bayesian4/batch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copy/paste this job script into a text file and submit with the command:
+#    sbatch thefilename
+# job standard output will go to the file slurm-%j.out (where %j is the job ID)
+
+#SBATCH --time=32:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=16   # 16 processor core(s) per node 
+#SBATCH --mem=32G   # maximum memory per node
+#SBATCH --job-name="mrbayes_cooking"
+#SBATCH --mail-user=mazeller@iastate.edu   # email address
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module load mrbayes
+
+mb go.nex
